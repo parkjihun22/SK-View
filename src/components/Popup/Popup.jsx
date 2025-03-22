1456bbbb3e38c95ec2a29492771a7d0072d532c8
parkjihun22 <eyeful31@naver.com>
2025-03-22 19:26:41 +0900

    feat : 헤더랑 팝업 수정

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -18,23 +18,24 @@ const Popup = () => {
   // 팝업이 열려있는지 여부를 추적하는 상태
   const [isClick, setIsClick] = useState(false);
 
-  // 페이지 로드 시 2초 후 팝업이 열리도록 useEffect 사용
+  // 페이지 로드 시 2초 후 팝업이 열리도록 하는 코드 → 잠시 주석 처리
+  /*
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsClick(true); // 2초 후에 팝업 자동 열기
-    }, 3000); // 2초 후에 실행
+    }, 3000); // 3초 후에 실행
 
-    // 클린업 함수 (컴포넌트가 언마운트될 때 타이머를 정리)
     return () => clearTimeout(timer);
-  }, []); // 빈 배열을 전달하여 컴포넌트 마운트 시 한 번만 실행
+  }, []);
+  */
 
   return (
     <div className={styles.container}>
       {isClick &&
-        <div className={styles.imgContainer} >
+        <div className={styles.imgContainer}>
           {popupArray.map((value, idx) => (
             <img
-              key={idx}  // key를 넣어야 배열 렌더링 시 효율적으로 동작
+              key={idx}
               className={styles.popupImg}
               src={value.img}
               alt={`progio-popup-image-${idx}`}
@@ -44,7 +45,10 @@ const Popup = () => {
       }
       <div className={styles.openPopupBtn} onClick={() => setIsClick(!isClick)}>
         <div className={styles.btnIcon}>
-          {isClick ? <FaAngleLeft size={20} color={"#FFFFFF"} /> : <FaAngleRight size={20} color={"#FFFFFF"} />}
+          {isClick
+            ? <FaAngleLeft size={20} color={"#FFFFFF"} />
+            : <FaAngleRight size={20} color={"#FFFFFF"} />
+          }
         </div>
         <div className={styles.btnText}>
           POPUP
